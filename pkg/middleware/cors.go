026ed50215efccad5b0b7d64bf7970fcf4e40186
Ovenoboyo <ovenoboyo@gmail.com>
2020-09-25 01:11:40 +0530

    Annotations for cors middleware

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// CorsMiddleware adds cors headers to response
 func CorsMiddleware() mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
